e056332ae98ead471176345f57c0facc87e026e7
Never、C <caolin>
2016-11-26 18:34:07 +0800

    init dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+"use strict";
+
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create();
+
+var webroot = "./dist/";
+
+var paths = {
+    js: webroot + "js/**/*.js",
+    minJs: webroot + "js/**/*.min.js",
+    css: webroot + "css/**/*.css",
+    minCss: webroot + "css/**/*.min.css",
+    concatJsDest: webroot + "js/site.min.js",
+    concatCssDest: webroot + "css/site.min.css"
+};
+
+gulp.task("min:js", function() {
+    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function() {
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+
 
 // Static server
 gulp.task('serve', function() {
@@ -11,4 +41,16 @@ gulp.task('serve', function() {
 
     gulp.watch("*.html").on('change', browserSync.reload);
 });
+
+//1. Less编译 压缩 合并
+
+gulp.task('css', function() {
+    return gulp.src([paths.css])
+        .pipe(less())
+        .pipe(gulp.dest(paths.css));
+});
+
+//2. JS合并 压缩 混淆
+
+//3. html压缩
 
